121bfb19ad96bf2dc567f1669acb7eed1f092253
alihangudenoglu <alihan_135@hotmail.com>
2024-11-29 00:26:38 +0300

    Migrations and ApplicationBuilderExtensions.

--- src/NoobGGApp.WebApi/Program.cs
+++ src/NoobGGApp.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using NoobGGApp.Application;
+using NoobGGApp.Infrastructure;
+using NoobGGApp.WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,5 +28,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.ApplyMigrations();
 
 app.Run();
